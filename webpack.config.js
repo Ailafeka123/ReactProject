d0b0f4020d44e20c7487ad580cc4f499b69c5dad
Ailafeka <ailafeka@gmail.com>
2025-03-25 13:11:25 +0800

    增加個人圖片以及更新image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { text } = require('stream/consumers');
 module.exports = {
     entry:{
         index:'./src/index.js',
@@ -21,6 +22,38 @@ module.exports = {
                         ],  
                     },
                 },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                use:[
+                    {
+                        loader:'image-webpack-loader',
+                        options:{
+                            mozjpeg:{
+                                progressive: true,
+                                quality:75, //JPEG壓縮
+                            },
+                            optipng:{
+                                enabled:true,
+                            },
+                            pngquant:{
+                                quality:[0.65, 0.9],//PNG壓縮
+                                speed:4,
+                            },
+                            gifsicle:{
+                                interlaced:false,
+                            },
+                            svgo:{
+                                plugins:[
+                                    {
+                                        removeViewBox:false,
+                                    },
+                                ],
+                            },
+                        },
+                    },
+                ],
             },
         ],
     },
